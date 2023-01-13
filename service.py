93fa2ebf07f2362b1d4949a8e57dbf2973cad120
jefedcreator <jefiene77@gmail.com>
2023-01-13 18:38:33 +0100

    optimizations

--- service.py
+++ service.py
@@ -8,7 +8,6 @@ class ProductService:
         print("discounts", discounts)
         for product in products:
             applicable_discounts = [d for d in discounts if (d.sku == product.sku) or (d.category == product.category)]
-            print("applicable_discounts", applicable_discounts)
             if applicable_discounts:
                 max_discount = max(applicable_discounts, key=lambda x: x.percentage)
                 product.price = {
@@ -27,22 +26,11 @@ class ProductService:
         return products
 
     def filter_products_by_category(products : List[Product], query: str) -> List[Product]:
-        filtered_product = []
-        for product in products:
-            if(product["category"] == query):
-                filtered_product.append(product)
-        return filtered_product
+        return [product for product in products if product["category"] == query]
 
     def filter_products_by_price(products : List[Product], query: int) -> List[Product]:
-        filtered_product = []
-        for product in products:
-            if(int(product["price"]) <= query):
-                filtered_product.append(product)
-        return filtered_product
+        return [product for product in products if int(product["price"]) <= query]
 
     def filter_products_by_price_category(products : List[Product], price_query: int, query : str) -> List[Product]:
-        filtered_product = []
-        for product in products:
-            if(int(product["price"]) <= price_query and product["category"] == query):
-                filtered_product.append(product)
-        return filtered_product
+        return [product for product in products if int(product["price"]) <= price_query and product["category"] == query]
+
